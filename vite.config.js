24faa4126fa58a5ed9744856263a8d9b869333be
mrHumildad <nghizzah@gmail.com>
2025-06-26 11:09:42 +0200

    qr update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CV_react/',
+  base: './',
+  build: {
+    outDir: 'dist', // Output directory
+    sourcemap: true // Generate source maps
+  }
 })
